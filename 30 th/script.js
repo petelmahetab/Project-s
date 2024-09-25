e9563ff1fd2347611da4290c7da173e3500fc592
Mahe2patel <patelmahetab9020@gmail.com>
2024-09-26 01:29:58 +0530

    Some change remaining

---
+++ 30 th/script.js
@@ -0,0 +1,46 @@
+// Js part select child element
+var ele=document.querySelector('.element');
+// console.log('ele')
+console.log(ele)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var eleImage=document.querySelector('#ele img');
+
+// // add eventListener
+// ele.addEventListener('mousemove',function(moves){
+//  eleImage.style.left=moves.x+'px';
+//  eleImage.style.top=moves.y+'px';
+// })
+
+// ele.addEventListener('mouseenter',function(moves){
+//     eleImage.style.opacity=1;
+//    })
+
+//    ele.addEventListener('mouseleave',function(moves){
+//     eleImage.style.opacity=0;
+//    })
