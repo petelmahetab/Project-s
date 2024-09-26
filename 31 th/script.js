5f38abff14bce09c4f97f5f150b21cb5414be8d6
Mahe2patel <patelmahetab9020@gmail.com>
2024-09-26 13:48:06 +0530

    Js Output Based Question's on Interview

---
+++ 31 th/script.js
@@ -0,0 +1,13 @@
+console.log("Starting String")
+import { variable } from "./script1.js";
+// import {add} from "./script1.js";
+// // create an new variable new File and import it into main script
+// console.log(variable);
+// console.log(add);
+
+
+// Checking an Scope of Variable
+// "use strict";
+ let Anything=1000;
+ console.log(Anything);
+ debugger;
---
+++ 31 th/script1.js
@@ -0,0 +1,9 @@
+ console.log("My name is Avinash");
+ console.log("Second Statement")
+ const variable="Container";
+ function Mod(n1,n2){
+     return n1%n2;
+ }
+// const add=Mod(10,2);
+ export{variable};
+// // // export{add};
