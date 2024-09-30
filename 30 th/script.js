e1cfb5a75a05efe51a71480a0aaf115fc1bdeaea
Mahe2patel <patelmahetab9020@gmail.com>
2024-09-30 23:52:32 +0530

    Movement on mouse project will be ready

--- 30 th/script.js
+++ 30 th/script.js
@@ -1,7 +1,24 @@
 // Js part select child element
-var ele=document.querySelector('.element');
+var ele=document.querySelectorAll('.element');
 // console.log('ele')
-console.log(ele)
+//Foreach for multiple element or list selected is by 
+ele.forEach(element => {
+  
+  element.addEventListener('mouseenter',function(){
+    element.childNodes[3].style.opacity=1;
+  });
+//event MouseEnter means were mouse will be on element
+  element.addEventListener('mouseleave',function(){
+    element.childNodes[3].style.opacity=0;
+      });
+      //event MouseLeave means were mouse will be leave from the 
+//MouseMove on
+      element.addEventListener('mousemove',function(d){
+        element.childNodes[3].style.left=d.x+"px";
+        element.childNodes[3].style.top=d.y+"px";
+          });
+
+});
 
 
 
