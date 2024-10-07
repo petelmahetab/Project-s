14cf6c3df8a863222b5a63a37048d67de3eb287c
Mahe2patel <patelmahetab9020@gmail.com>
2024-10-08 01:00:49 +0530

    50% done

---
+++ 40 th/JS.js
@@ -0,0 +1,11 @@
+
+const textArea=document.getElementById('textarea');
+// console.log("first"+textArea);
+const count=document.getElementById('count');
+textArea.addEventListener('keyup',(val)=>{
+    
+})
+
+function updateCounter(){
+    textArea.ariaValueMax.length
+}
