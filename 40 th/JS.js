c27c8593831a048353c6b44e3723b9268a6b8434
Mahe2patel <patelmahetab9020@gmail.com>
2024-10-09 23:42:35 +0530

    Character Counter will be done

--- 40 th/JS.js
+++ 40 th/JS.js
@@ -2,10 +2,14 @@
 const textArea=document.getElementById('textarea');
 // console.log("first"+textArea);
 const count=document.getElementById('count');
+const remain=document.getElementById('values');
 textArea.addEventListener('keyup',(val)=>{
-    
+    updateCounter();
 })
 
 function updateCounter(){
-    textArea.ariaValueMax.length
+    count.textContent=textArea.value.length;
+    // we get an Attribute of an Text area and subtract with the length of value is inserted in TEXTAREA //
+// bedfault then maxlength will be :- >  100
+   remain.innerText=textArea.getAttribute('maxlength')-textArea.value.length;
 }
