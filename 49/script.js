69c73a6a6fd38a33290d84726b4c47c175ba1ada
Mahe2patel <patelmahetab9020@gmail.com>
2024-10-28 20:47:05 +0530

    JS for Google Search Bar

---
+++ 49/script.js
@@ -0,0 +1,9 @@
+
+const getClass=document.querySelector('.Container');
+const Listen=document.querySelector('.mike');
+Listen.addEventListener('click',()=>{
+  alert("Use your microphone ");  
+})
+const Lens=document.querySelector('.lens');
+Lens.addEventListener('click',()=>{
+    confirm("Drag image here or Upload file")})
