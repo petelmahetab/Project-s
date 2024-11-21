202a3f8b478b0d0a025668e01cfc867869d854e6
Mahe2patel <patelmahetab9020@gmail.com>
2024-11-21 23:32:57 +0530

    UI done

---
+++ 60/Js.js
@@ -0,0 +1,37 @@
+// get the all Elements
+
+const timer=document.querySelector('#timer')
+const startButton=document.querySelector('#start')
+const resetButton=document.querySelector('#reset')
+const stopButton=document.querySelector('#stop')
+
+
+
+
+// Function for the start the timer
+
+let startTime=0
+let elaspedTime=0
+let timerKaInterval;
+
+// .Date lenge
+function startTimer(){
+    console.log("first")
+}
+
+
+
+startButton.addEventListener('click',()=>{
+ console.log("started")
+})
+
+resetButton.addEventListener('click',()=>{
+    console.log("reseted")
+   })
+   
+   stopButton.addEventListener('click',()=>{
+    console.log("stopped")
+   })
+   
+   
+   
