3682e12013c843b8be4bcd8652b0d90f2ed7bd35
Mahe2patel <patelmahetab9020@gmail.com>
2024-11-26 23:00:07 +0530

    All js code will be done..!

--- 60/Js.js
+++ 60/Js.js
@@ -5,32 +5,59 @@ const startButton=document.querySelector('#start')
 const resetButton=document.querySelector('#reset')
 const stopButton=document.querySelector('#stop')
 
-
-
-
 // Function for the start the timer
 
 let startTime=0
 let elaspedTime=0
 let timerKaInterval;
 
-// .Date lenge
+// .Date lenge fir osme se elaspedTime -
 function startTimer(){
-    console.log("first")
+   startTime=Date.now()-elaspedTime
+
+   timerKaInterval=setInterval(()=>{
+       elaspedTime=Date.now()-startTime
+
+       timer.textContent=formatTime(elaspedTime)
+   }, 10)
+}
+
+// Formatting or getting Hr,minutes ....
+function formatTime(elaspedTime)
+{
+   const miliSe=Math.floor((elaspedTime%1000)/10);
+ const getSec=Math.floor((elaspedTime/1000)%60);
+ const getMin=Math.floor((elaspedTime/(1000*60))%60);
+ const getHr=Math.floor((elaspedTime/(1000*60*60)))
+   
+//  retuning an time as String and to padStart(2,'0') method it will convert Number  --> String
+return `${String(getHr).padStart(2,'0')}:${String(getMin).padStart(2,'0')}:${String(getSec).padStart(2,'0')}:${String(miliSe).padStart(2,'0')}`;
+ 
 }
 
 
+// Function for Stop and Reset Timer of clock
+function stopTimer(){
+    clearInterval(timerKaInterval);
+}
+
+function resetTimer(){
+    clearInterval(timerKaInterval);
+    elaspedTime=0;
+    timer.innerHTML='00:00:00';
+}
 
 startButton.addEventListener('click',()=>{
- console.log("started")
+
+    startTimer();
 })
 
 resetButton.addEventListener('click',()=>{
-    console.log("reseted")
+    resetTimer();
    })
    
    stopButton.addEventListener('click',()=>{
-    console.log("stopped")
+    stopTimer();
    })
    
    
